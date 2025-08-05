12cdd0f534901095bf21017abce18db06179e574
Nguyen Sy Nguyen <nguyensynguyen7856@gmail.com>
2025-08-05 09:17:18 +0700

    Create BasicVariableDeclaration.java

---
+++ 1. Learn the Basics/Data types/BasicVariableDeclaration.java
@@ -0,0 +1,25 @@
+public class BasicVariablesDeclaration {
+    public static void main(String[] args) {
+        // Integer variable
+        int age = 25;
+        
+        // Double variable
+        double price = 19.99;
+        
+        // Boolean variable
+        boolean isJavaFun = true;
+        
+        // Character variable
+        char grade = 'A';
+        
+        // String variable
+        String name = "Alice";
+
+        System.out.println("Integer: " + age);
+        System.out.println("Double: " + price);
+        System.out.println("Boolean: " + isJavaFun);
+        System.out.println("Character: " + grade);
+        System.out.println("String: " + name);
+    }
+}
+
